54df7e2c5c6bfeb7f3f52363be85ac0ba943801d
DevIncomin <56730075+Developer-Incoming@users.noreply.github.com>
2021-10-31 21:31:02 +0300

    Improvements And Cleaning-
    Improved code, you can call the play function with "loopFor" variable to 0 and it'll loop forever, probably.
    Cleaner code, now it's easier to read.
    Removed unnecessary modules that've been used for testing.

--- Mic Audio Player/main.py
+++ Mic Audio Player/main.py
@@ -1,20 +1,26 @@
 import time
 
 import pygame
-from pygame import USEREVENT
 from pygame import mixer
-from pygame._sdl2 import get_num_audio_devices, get_audio_device_name
 
 def play(path, volume, loopFor, cooldown):
     mixer.init(devicename="CABLE Input (VB-Audio Virtual Cable)")
-
     mixer.music.load(path)
     mixer.music.set_volume(volume)
-    for i in range(loopFor):
-        mixer.music.play()
-        while mixer.music.get_busy():
-            pass
-        time.sleep(cooldown)
+    
+    if loopFor == 0:
+        for i in range(loopFor):
+            mixer.music.play()
+            while mixer.music.get_busy():
+                pass
+            time.sleep(cooldown)
+    else:
+        while True:
+            mixer.music.play()
+            while mixer.music.get_busy():
+                pass
+            time.sleep(cooldown)
+    
     mixer.music.unload()
 
-play("audio.mp3", 0.25, 3, 2)
+play("audio.mp3", 0.5, 3, 2)
