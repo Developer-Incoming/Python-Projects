6f6ecff9e00cd76aaf265484b7a5c48b19ee3171
DevIncomin <56730075+Developer-Incoming@users.noreply.github.com>
2022-09-21 09:17:14 +0300

    Released main files
    surfer.py surfs the cocktails page and fetches all cocktail links, which are already done in yay.txt, but have been processed with a simple txt replacer in NotePad++ and now in completeCocktailLinks.txt.
    
    sitesSaver.py saves any file and edits a bit of the css.css file to accept the file that I did provide so you can view the page way better.
    I haven't provided all of the sites that have been saved but a few because it's too large for GitHub 💀.

---
+++ sitesSaver.py
@@ -0,0 +1,42 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+import numpy as np
+
+# from bs4 import BeautifulSoup
+
+cdPath = "D:\chromedriver\chromedriver.exe"
+driver = webdriver.Chrome(executable_path=cdPath)
+
+# nextCocktailButton = driver.find_element_by_class_name("btn btn-block btn-sm btn-success")
+# nextCocktailButton.click()
+# .getElementsByClassName("col-12 btn shadow-sm mb-2 text-left")[0].href
+# document.querySelector("#swipeme > div:nth-child(3) > div:nth-child(2) > div.row.justify-content-sm-center > div:nth-child(1)")
+# driver.find_element(By.CLASS_NAME, "")
+
+cocktailsPages = open("D:\Bader's Stuff!\Programming\Python\Python Files\Python Files [2022]\Cocktails Surfer\completeCocktailLinks.txt", "r").read().split("\n")
+
+def openPage(url):
+    driver.get(url)
+
+def savePage(pageSpecialName):
+    # print("yas, save page")
+
+    # driver.find_element(By.CSS_SELECTOR, "head > link:nth-child(42)")[0].setAttribute("href", "css.css?v=20229")
+    driver.execute_script("arguments[0].setAttribute('href',arguments[1])", driver.find_element(By.CSS_SELECTOR, "head > link:nth-child(42)"), "css.css?v=20229")
+    content = driver.page_source
+    # print(content)
+
+    # soup = BeautifulSoup(content)
+    file = open("D:\Bader's Stuff!\Programming\Python\Python Files\Python Files [2022]\Cocktails Surfer\Cocktails Pages\\" + pageSpecialName + ".html", "w", encoding="utf-8")
+    file.write(content)
+
+def main():
+    for pageLink in cocktailsPages[3346:]:
+        openPage(pageLink)
+        savePage(pageLink.split("/")[5])
+    
+
+main()
+
+driver.close()
